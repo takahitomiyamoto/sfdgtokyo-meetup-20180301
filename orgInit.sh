9d2e51e528a9d76de75c47e3b9f5f0490cc7d7e6
takahito.miyamoto <takahito.miyamoto@accenture.com>
2018-03-01 15:49:17 +0900

    change orgInit

--- orgInit.sh
+++ orgInit.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-echo meetup へ移動します
+echo
+echo meetup へ移動します ...
 cd meetup
 
-echo scratch org を作成します
+echo
+echo scratch org を作成します ...
 sfdx force:org:create -s -f config/project-scratch-def.json -a feature01
 
-echo org の一覧を表示します
+echo
+echo org の一覧を表示します ...
 sfdx force:org:list
 
 # echo ユーザを作成します
 # sfdx force:user:create -a admin-user -f config/user-def.json
 
-echo ソースコードを push します
+echo
+echo ソースコードを push します ...
 sfdx force:source:push
 
-echo 権限セットを割り当てます
+echo
+echo 権限セットを割り当てます ...
 sfdx force:user:permset:assign -n SS_New_Component_List
 
-echo scratch org へアクセスします
+echo
+echo scratch org へアクセスします ...
 sfdx force:org:open -p lightning/n/SS_New_Component_List
